bd256a83d86390b31bec575873d3493e5c0b6182
Dimazavr78 <dimazavr78@gmail.com>
2026-08-24 22:49:15 +0300

    First commit Start proeckt

---
+++ back/src/index.ts
@@ -0,0 +1,27 @@
+import Fastify from "fastify";
+
+const app = Fastify({
+    logger: true
+});
+
+app.get("/api/health", async () => {
+    return {
+        status: "ok",
+        application: "LeatherCAD",
+        version: "0.0.1"
+    };
+});
+
+const start = async () => {
+    try {
+        await app.listen({
+            port: 3000,
+            host: "127.0.0.1"
+        });
+    } catch (error) {
+        app.log.error(error);
+        process.exit(1);
+    }
+};
+
+start();
